dfa1b9019ff96d8f21e0d84b5b7060815a20bc33
DillonCallinan <163197558+DillonCallinan@users.noreply.github.com>
2026-04-21 13:56:02 +0100

    Problems 6, 7, 8, 9, 11

--- challenges/ProjectEuler/solved/3/solution.py
+++ challenges/ProjectEuler/solved/3/solution.py
@@ -1,32 +1,3 @@
-from typing import Iterator
-
-
-# Not used in this solution
-class Primes(Iterator[int]):
-    _primes: set[int]
-    _i: int
-    _max: int
-
-    def __init__(self, max: int = 10_000) -> None:
-        self._primes = set()
-        self._i = 0
-        self._max = max
-
-    def __next__(self) -> int:
-        return self._next()
-
-    def _next(self) -> int:
-        self._i += 1
-        if self._i > self._max:
-            raise StopIteration
-
-        if is_prime(self._i):
-            self._primes.add(self._i)
-            return self._i
-
-        return self._next()
-
-
 def is_factor(a: int, b: int) -> bool:
     """
     Returns True if b is a factor of a, otherwise returns False.
